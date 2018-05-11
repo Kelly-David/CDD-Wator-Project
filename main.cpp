9721cf585c491123544b24f0791aaf18e18d0f7d
kelly-david <davidkelly.tlf@gmail.com>
2018-05-11 14:14:59 +0100

    Removing test code

--- main.cpp
+++ main.cpp
@@ -26,13 +26,12 @@
 #include <ctime>
 #include <fstream>
 
-int const _X = 100;      // Ocean width
-int const _Y= 100;       // Ocean height
-int const LIMIT = 100;   // Must match array bounds, expects ocean to be square
+int const _X = 100;     // Ocean width
+int const _Y = 100;     // Ocean height
+int const LIMIT = 100;  // Must match array bounds, expects ocean to be square
 char const WATER = ' '; // Water is blank space
 char const FISH = '.';  // Fish are o
 char const SHARK = '$'; // Sharks are $
-
 /**
  * Create 2d arrays representing ocean; breeding time; starving time
  * ocean, breed, starve are for display
@@ -48,9 +47,9 @@ int starveNext[_X][_Y];
 std::vector<int> neighbours;
 
 int totalFish = 10;
-int allFish = totalFish;    // Running count of fish
+int allFish = totalFish; // Running count of fish
 int totalSharks = 50;
-int allSharks = totalSharks;// Running count of sharks
+int allSharks = totalSharks; // Running count of sharks
 
 int breedLife = 5;
 int starveLife = 5;
@@ -65,10 +64,13 @@ long int microseconds = 100000; // Timer variable
  * breed arrays with 0
  * starve arrays with 0
  */
-void populateAllArrays() {
-    #pragma omp parallel for num_threads(2)
-    for (int i = 0; i < _X; ++i) {
-        for (int k = 0; k < _Y; ++k) {
+void populateAllArrays()
+{
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < _X; ++i)
+    {
+        for (int k = 0; k < _Y; ++k)
+        {
             ocean[i][k] = WATER;
             oceanNext[i][k] = WATER;
             breed[i][k] = breedLife;
@@ -83,11 +85,13 @@ void populateAllArrays() {
  * \brief void populateWithFish() :: Randomly places Fish in the ocean array
  * Sets corresponding breed/starve arrays
  */
-void populateWithFish() {
-    #pragma omp parallel for num_threads(2)
-    for (int i = 0; i < totalFish; ++i) {
-        randomXPos = (int) random() % LIMIT;
-        randomYPos = (int) random() % LIMIT;
+void populateWithFish()
+{
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < totalFish; ++i)
+    {
+        randomXPos = (int)random() % LIMIT;
+        randomYPos = (int)random() % LIMIT;
         ocean[randomXPos][randomYPos] = FISH;
         breed[randomXPos][randomYPos] = breedLife;
         starve[randomXPos][randomXPos] = starveLife;
@@ -98,11 +102,13 @@ void populateWithFish() {
  * \brief void populateWithSharks() :: Randomly places Sharks in the ocean array
  * Sets corresponding breed/starve arrays
  */
-void populateWithSharks() {
-    #pragma omp parallel for num_threads(2)
-    for (int i = 0; i < totalSharks; ++i) {
-        randomXPos = (int) random() % LIMIT;
-        randomYPos = (int) random() % LIMIT;
+void populateWithSharks()
+{
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < totalSharks; ++i)
+    {
+        randomXPos = (int)random() % LIMIT;
+        randomYPos = (int)random() % LIMIT;
         ocean[randomXPos][randomYPos] = SHARK;
         breed[randomXPos][randomYPos] = breedLife;
         starve[randomXPos][randomYPos] = starveLife;
@@ -113,24 +119,27 @@ void populateWithSharks() {
  * \brief Copies the contents of ...next arrays to display arrays
  */
 void updateOceanContents(
-        char toOcean[LIMIT][LIMIT], char fromOcean[LIMIT][LIMIT],
-        int toBreed[LIMIT][LIMIT], int fromBreed[LIMIT][LIMIT],
-        int toStarve[LIMIT][LIMIT], int fromStarve[LIMIT][LIMIT]) {
-    #pragma omp parallel for num_threads(2)
-    for (int i = 0; i < LIMIT; ++i) {
-        for (int k = 0; k < LIMIT; ++k) {
+    char toOcean[LIMIT][LIMIT], char fromOcean[LIMIT][LIMIT],
+    int toBreed[LIMIT][LIMIT], int fromBreed[LIMIT][LIMIT],
+    int toStarve[LIMIT][LIMIT], int fromStarve[LIMIT][LIMIT])
+{
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < LIMIT; ++i)
+    {
+        for (int k = 0; k < LIMIT; ++k)
+        {
             toOcean[i][k] = fromOcean[i][k];
             toBreed[i][k] = fromBreed[i][k];
             toStarve[i][k] = fromStarve[i][k];
         }
     }
-
 }
 
 /**
  * \brief void create() :: Populates the ocean array with respective characters
  */
-void create() {
+void create()
+{
     populateAllArrays();
     populateWithFish();
     populateWithSharks();
@@ -140,16 +149,21 @@ void create() {
 /**
  * \brief Update totals for display
  */
-void updateTotals() {
+void updateTotals()
+{
     allSharks = 0;
     allFish = 0;
-    #pragma omp parallel for num_threads(2)
-    for (int i = 0; i < LIMIT; ++i) {
-        for (int k = 0; k < LIMIT; ++k) {
-            if (ocean[i][k] == FISH) {
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < LIMIT; ++i)
+    {
+        for (int k = 0; k < LIMIT; ++k)
+        {
+            if (ocean[i][k] == FISH)
+            {
                 ++allFish;
             }
-            if (ocean[i][k] == SHARK) {
+            if (ocean[i][k] == SHARK)
+            {
                 ++allSharks;
             }
         }
@@ -159,7 +173,8 @@ void updateTotals() {
 /**
  * \brief Breed Fish
  */
-void breedFish(int xpos, int ypos) {
+void breedFish(int xpos, int ypos)
+{
     // New fish are placed in the pre-move location of parent.
     oceanNext[xpos][ypos] = FISH;
     breedNext[xpos][ypos] = breedLife;
@@ -171,7 +186,8 @@ void breedFish(int xpos, int ypos) {
  * \brief Eat Fish
  * Removes the Fish from the oceanNext array at the corresponding position
  */
-void eatFish(int xpos, int ypos) {
+void eatFish(int xpos, int ypos)
+{
     oceanNext[xpos][ypos] = WATER;
     breedNext[xpos][ypos] = breedLife;
     starveNext[xpos][ypos] = starveLife;
@@ -184,7 +200,8 @@ void eatFish(int xpos, int ypos) {
 /**
  * \brief Kill Shark
  */
-void killShark( int xpos, int ypos) {
+void killShark(int xpos, int ypos)
+{
     oceanNext[xpos][ypos] = WATER;
     breedNext[xpos][ypos] = breedLife;
     starveNext[xpos][ypos] = starveLife;
@@ -196,9 +213,10 @@ void killShark( int xpos, int ypos) {
 /**
  * \brief int randomGen(long int limit)::returns a random integer between 1 and limit
  */
-int randomGen(long int range) {
-    std::random_device rd; // obtain a random number from hardware
-    std::mt19937 eng(rd()); // seed the generator
+int randomGen(long int range)
+{
+    std::random_device rd;                           // obtain a random number from hardware
+    std::mt19937 eng(rd());                          // seed the generator
     std::uniform_int_distribution<> distr(1, range); // define the range
 
     return distr(eng);
@@ -207,32 +225,38 @@ int randomGen(long int range) {
 /**
  * \brief int getMoveDirection(
  */
-int getMoveDirection(char type, int xpos, int ypos) {
+int getMoveDirection(char type, int xpos, int ypos)
+{
 
     // Reset the neighbours vector
     neighbours.clear();
 
-    if (type == SHARK) {
-        if(oceanNext[xpos][(ypos + LIMIT - 1) % LIMIT] == FISH) {
+    if (type == SHARK)
+    {
+        if (oceanNext[xpos][(ypos + LIMIT - 1) % LIMIT] == FISH)
+        {
             // Fish is North
             neighbours.push_back(1);
             // Remove the fish
             eatFish(xpos, (ypos + LIMIT - 1) % LIMIT);
             // return 1;
         }
-        else if (oceanNext[xpos][(ypos + 1) % LIMIT] == FISH){
+        else if (oceanNext[xpos][(ypos + 1) % LIMIT] == FISH)
+        {
             // Fish is South
             neighbours.push_back(2);
             eatFish(xpos, (ypos + 1) % LIMIT);
             // return 2;
         }
-        else if (oceanNext[(xpos + 1) % LIMIT][ypos] == FISH){
+        else if (oceanNext[(xpos + 1) % LIMIT][ypos] == FISH)
+        {
             //Fish is East
             neighbours.push_back(3);
             eatFish((xpos + 1) % LIMIT, ypos);
             // return 3;
         }
-        else if (oceanNext[(xpos + LIMIT - 1) % LIMIT][ypos] == FISH){
+        else if (oceanNext[(xpos + LIMIT - 1) % LIMIT][ypos] == FISH)
+        {
             // Fish is West
             neighbours.push_back(4);
             eatFish((xpos + LIMIT - 1) % LIMIT, ypos);
@@ -240,32 +264,42 @@ int getMoveDirection(char type, int xpos, int ypos) {
         }
     }
     // If it's a FISH or ( a SHARK and the neighbours is still empty )
-    if ((type == FISH ) || ((type == SHARK) && (neighbours.empty()))) {
+    if ((type == FISH) || ((type == SHARK) && (neighbours.empty())))
+    {
         // If it's a SHARK it hasn't found a fish so starve it...
-        if (type == SHARK) { ocean[xpos][ypos]--; }
+        if (type == SHARK)
+        {
+            ocean[xpos][ypos]--;
+        }
 
-        if (oceanNext[xpos][(ypos + LIMIT - 1) % LIMIT] == WATER){
+        if (oceanNext[xpos][(ypos + LIMIT - 1) % LIMIT] == WATER)
+        {
             // North
             neighbours.push_back(1);
         }
-        if (oceanNext[xpos][(ypos + 1) % LIMIT] == WATER){
+        if (oceanNext[xpos][(ypos + 1) % LIMIT] == WATER)
+        {
             // South
             neighbours.push_back(2);
         }
-        if (oceanNext[(xpos + 1) % LIMIT][xpos] == WATER){
+        if (oceanNext[(xpos + 1) % LIMIT][xpos] == WATER)
+        {
             // East
             neighbours.push_back(3);
         }
-        if (oceanNext[(xpos + LIMIT - 1) % LIMIT][ypos] == WATER){
+        if (oceanNext[(xpos + LIMIT - 1) % LIMIT][ypos] == WATER)
+        {
             //West
             neighbours.push_back(4);
         }
     }
-    if (neighbours.empty()) {
+    if (neighbours.empty())
+    {
         // Animal cannot move
         return 0;
     }
-    else {
+    else
+    {
         // Select a random direction
         int direction = randomGen(neighbours.size());
         return neighbours[direction];
@@ -276,7 +310,8 @@ int getMoveDirection(char type, int xpos, int ypos) {
  * \brief Moves the Fish or Sharks to their new location
  * Moves the contents of array at xpos and ypos to the index of array ...next at xMove yMove
  */
-void move(char animal, int xpos, int ypos, int xMove, int yMove) {
+void move(char animal, int xpos, int ypos, int xMove, int yMove)
+{
 
     oceanNext[xMove][yMove] = animal;
     breedNext[xMove][yMove] = breed[xpos][ypos];
@@ -289,59 +324,77 @@ void move(char animal, int xpos, int ypos, int xMove, int yMove) {
 /**
  * \brief Prints the ocean array to screen
  */
-void simulate() {
+void simulate()
+{
     int moveDirection = 0;
     bool canBreed = false;
-    for (int i = 0; i < LIMIT; ++i) {
-        for (int k = 0; k < LIMIT; ++k) {
+    for (int i = 0; i < LIMIT; ++i)
+    {
+        for (int k = 0; k < LIMIT; ++k)
+        {
             // If it's a FISH...
-            if ((ocean[i][k] == FISH)) {
+            if ((ocean[i][k] == FISH))
+            {
                 // Find the move direction. Will be 0-4
                 moveDirection = getMoveDirection(FISH, i, k);
 
-                if (moveDirection != 0) {
+                if (moveDirection != 0)
+                {
                     //Fish can move - decrement breed time
                     breed[i][k]--;
-                    if (breed[i][k] == 0) {
+                    if (breed[i][k] == 0)
+                    {
                         // Fish can breed...
                         // Make a new fish in the current fish location in both ocean arrays
                         canBreed = true;
                     }
                 }
-                if (moveDirection == 1) { // North: decrease Y
+                if (moveDirection == 1)
+                { // North: decrease Y
 
                     move(FISH, i, k, i, (k + LIMIT - 1) % LIMIT);
-
-                } else if (moveDirection == 2) { // South: Increase Y
+                }
+                else if (moveDirection == 2)
+                { // South: Increase Y
 
                     move(FISH, i, k, i, (k + 1) % LIMIT);
-
-                } else if (moveDirection == 3) { // East: Increase X
+                }
+                else if (moveDirection == 3)
+                { // East: Increase X
 
                     move(FISH, i, k, (i + 1) % LIMIT, k);
-
-                } else if (moveDirection == 4) { // West: Decrease X
+                }
+                else if (moveDirection == 4)
+                { // West: Decrease X
 
                     move(FISH, i, k, (i + LIMIT - 1) % LIMIT, k);
                 }
-                if (canBreed) { breedFish(i,k); }
+                if (canBreed)
+                {
+                    breedFish(i, k);
+                }
             }
             // If its a SHARK...
-            else if ((ocean[i][k] == SHARK)) {
+            else if ((ocean[i][k] == SHARK))
+            {
                 moveDirection = getMoveDirection(SHARK, i, k);
 
-                if (moveDirection != 0) {
+                if (moveDirection != 0)
+                {
                     // Shark can move - decrement breed time
                     breed[i][k]--;
-                    if (breed[i][k] == 0) {
+                    if (breed[i][k] == 0)
+                    {
                         // Shark can breed - are sharks allowed to breed?
                     }
                 }
 
-                if (starve[i][k] == 0) {
+                if (starve[i][k] == 0)
+                {
                     killShark(i, k);
-
-                } else if (moveDirection == 1) { // North: decrease Y
+                }
+                else if (moveDirection == 1)
+                { // North: decrease Y
 
                     oceanNext[i][(k + LIMIT - 1) % LIMIT] = SHARK;
                     starveNext[i][(k + LIMIT - 1) % LIMIT] = starve[i][k];
@@ -350,8 +403,9 @@ void simulate() {
                     oceanNext[i][k] = WATER;
                     breedNext[i][k] = breedLife;
                     starveNext[i][k] = starveLife;
-
-                } else if (moveDirection == 2) { // South: Increase Y
+                }
+                else if (moveDirection == 2)
+                { // South: Increase Y
 
                     oceanNext[i][(k + 1) % LIMIT] = SHARK;
                     starveNext[i][(k + 1) % LIMIT] = starve[i][k];
@@ -360,8 +414,9 @@ void simulate() {
                     oceanNext[i][k] = WATER;
                     breedNext[i][k] = breedLife;
                     starveNext[i][k] = starveLife;
-
-                } else if (moveDirection == 3) { // East: Increase X
+                }
+                else if (moveDirection == 3)
+                { // East: Increase X
 
                     oceanNext[(i + 1) % LIMIT][k] = SHARK;
                     starveNext[(k + 1) % LIMIT][k] = starve[i][k];
@@ -370,8 +425,9 @@ void simulate() {
                     oceanNext[i][k] = WATER;
                     breedNext[i][k] = breedLife;
                     starveNext[i][k] = starveLife;
-
-                } else if (moveDirection == 4) { // West: Decrease X
+                }
+                else if (moveDirection == 4)
+                { // West: Decrease X
 
                     oceanNext[(i + LIMIT - 1) % LIMIT][k] = SHARK;
                     starveNext[(i + LIMIT - 1) % LIMIT][k] = starve[i][k];
@@ -381,7 +437,6 @@ void simulate() {
                     breedNext[i][k] = breedLife;
                     starveNext[i][k] = starveLife;
                 }
-
             }
         }
     }
@@ -391,10 +446,13 @@ void simulate() {
 /**
  * \brief Prints the ocean array to screen
  */
-void print() {
-    #pragma omp parallel for num_threads(2)
-    for (int i = 0; i < LIMIT; ++i) {
-        for (int k = 0; k < LIMIT; ++k) {
+void print()
+{
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < LIMIT; ++i)
+    {
+        for (int k = 0; k < LIMIT; ++k)
+        {
             std::cout << ocean[i][k];
         }
         std::cout << std::endl;
@@ -406,70 +464,38 @@ void print() {
 /**
  * \brief Main
  */
-int main() {
+int main()
+{
 
     /** 
      * Threads Setup
     */
     omp_set_dynamic(0);     // Explicitly disable dynamic teams
-    omp_set_num_threads(2); // Use 4 threads for all consecutive parallel regions
-    /** 
-     * Testing only
-     * ...since omp_get_num_threads() defaults to one in serial code.
-    */
-    #pragma omp parallel 
+    omp_set_num_threads(4); // Use 4 threads for all consecutive parallel regions
+
+    //Initialize random seed for random number generator
+    srand(time(NULL));
+
+    std::cout << "Wator World Simulation" << std::endl;
+
+    // Create the Wator world
+    create();
+
+    // Start main loop
+    do
     {
-        std::cout << "Threads= " << omp_get_num_threads() << std::endl;
-    }
-
-    // File system
-    std::ofstream output;
-    output.open ("2threads.csv");
-
-    // Benchmarking Loop
-    for (int i = 0; i < 10; ++i) {
-
-        //Initialize random seed for random number generator
-        srand (time(NULL));
-
-        std::cout << "Wator World Simulation" << std::endl;
-
-        // Create the Wator world
-        create();
-
-        // Start main loop
-        do {
-            /** 
-             * Benchmarking Timer 
-            */
-            std::clock_t start;
-            double duration;
-            start = std::clock();
-
-            // Prints the ocean
-            print();
-            std::cout << "--------------------------------------------------------------" << std::endl;
-            // Timer
-            usleep(microseconds);
-            // Simulate
-            simulate();
-            // Update the arrays
-            updateOceanContents(ocean, oceanNext, breed, breedNext, starve, starveNext);
-            // Update Shark and Fish totals
-            updateTotals();
-
-
-            // Get time difference
-            duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-            // Write to file
-            output << duration << "\n";
-
-
-        } while ((allSharks > 0) && (allFish > 0)); // Run until all animals are gone
-
-    } //loop end.
-
-    output.close();
+        // Prints the ocean
+        print();
+        std::cout << "--------------------------------------------------------------" << std::endl;
+        // Timer
+        usleep(microseconds);
+        // Simulate
+        simulate();
+        // Update the arrays
+        updateOceanContents(ocean, oceanNext, breed, breedNext, starve, starveNext);
+        // Update Shark and Fish totals
+        updateTotals();
+    } while ((allSharks > 0) && (allFish > 0)); // Run until all animals are gone
 
     return 0;
 }
